63d4404e1ac011c0cb374493975a462165ab93aa
chappers00 <4682866+chappers00@users.noreply.github.com>
2019-11-05 21:09:26 +0000

    Refreshing example with valid tests, updating dependencies

---
+++ README.md
@@ -0,0 +1,14 @@
+# REST-ASSURED-TEMPLATE
+This repo is a quickstart template for anyone wanting to get started with the REST Assured framework http://rest-assured.io/
+
+Currently the tests can be executed in IntelliJ or VSCode
+
+Gradle is the currently supported build tool.
+
+To execute the tests once the repo is cloned run the following command on Linux:
+
+```./gradlew test```
+
+or on Windows:
+
+```gradlew.bat test```
---
+++ src/test/java/apiTestExamples.java
@@ -0,0 +1,43 @@
+import org.junit.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+public class memberTest {
+
+    @Test() public void
+    //Also see https://any-api.com/xkcd_com/xkcd_com/docs/_info_0_json/GET
+    xkcdinfo0() {
+        when().get("http://xkcd.com/info.0.json").
+                then().
+                statusCode(200).
+                body("$", hasKey("title"));
+    }
+
+    @Test() public void
+    xkcdGetComic() {
+        when().get("http://xkcd.com/{comicId}/info.0.json", "386").
+        then().
+        statusCode(200).
+        body("safe_title", equalTo("Duty Calls"));
+    }
+
+    @Test
+    public void testJsonSchema() {
+            when()
+            .get("https://jsonplaceholder.typicode.com/comments?postId=1")
+            .then()
+            .assertThat()
+            .body(matchesJsonSchemaInClasspath("schema.jsd"));
+    }
+    @Test(expected = AssertionError.class)
+    public void testJsonSchemaFail() {
+            when()
+            .get("https://jsonplaceholder.typicode.com/comments?postId=1")
+            .then()
+            .assertThat()
+            .body(matchesJsonSchemaInClasspath("schemaFail.jsd"));
+    }
+    
+}
